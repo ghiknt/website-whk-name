f402f8bcbe92c38439aec71f56263dabe24e4974
https://whk.name/about/me/ (whk) <no-reply@whk.name>
2015-02-16 18:59:28 +0000

    Order TOC by reverse date and name

--- lib/helpers/utilities.rb
+++ lib/helpers/utilities.rb
@@ -5,7 +5,40 @@ module ErpetuUtilities
   def listchildren
    returnval = ""
    returnval += "<dl>"
-   item.children.each do |child|
+
+   children = item.children
+   sorted = children.sort{ |a, b|
+     if ( ! a[:modified].nil? ) 
+       a_date = a[:modified]
+     else
+       a_date = Date.jd(0)
+     end
+
+     if ( ! b[:modified].nil? ) 
+       b_date = b[:modified]
+     else
+       b_date = Date.jd(0)
+     end
+
+     if (a_date == b_date)
+      if ( ! a[:title].nil? ) 
+        a_title = a[:title]
+      else
+        a_title = a.identifier
+      end
+      if ( ! b[:title].nil? ) 
+        b_title = b[:title]
+      else
+        b_title = b.identifier
+      end
+      a_title <=> b_title
+     else
+      # Most recent on top
+      b_date <=> a_date
+     end
+
+   }
+   sorted.each do |child|
     if ! child[:toc].nil? && child[:toc] == "ignore"
       next
     elsif ! child.binary?
@@ -16,7 +49,11 @@ module ErpetuUtilities
      else
       returnval =returnval+child.identifier
      end
-     returnval =returnval+"</a></dt>"
+     returnval =returnval+"</a>"
+     if ! child[:modified].nil?
+      returnval =returnval+child[:modified].strftime(" (Modified %Y-%m-%d)")
+     end
+     returnval =returnval+"</dt>"
      if ! child[:summary].nil?
       returnval = returnval+"<dd>"+child[:summary] + "</dd>"
      end
