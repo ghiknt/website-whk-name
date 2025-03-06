806f6a61be7b46d4019f526e73a063b5579baf2d
https://whk.name/about/me/ (whk) <develop@whk.name>
2025-03-06 21:12:15 +0000

    WIP: Updating build file

--- config/publish/index.el
+++ config/publish/index.el
@@ -66,26 +66,36 @@
 	 :base-directory "~/dotfiles/"
 	 :publishing-directory "~/build/web/misc/dotfiles/"
 	 :recursive f
-	 :html-link-use-abs-url nil
-	 :html-postamble nil
-	 :html-preamble t
-	 :html-scripts nil
-	 :html-style t
-	 :html5-fancy t
-	 :tex t
-	 :html-doctype  html5
-	 :html-container  div
-	 :html-content-class  content
-	 ;; :html-mathjax
-	 :html-equation-reference-format  \eqref{%s}
-	 ;; :html-head 
-	 ;; :html-head_extra 
-	 ;; :infojs_opt 
-	 ;; :latex-header
-	 :html-link-home "../.."
-	 :html-link-up   "..")
-	("dotfiles" :components ("dotfiles-html"))
+	 ;; 
+	 )
+	("emacs.d-html"
+	 :base-extension "org"
+	 :publishing-function org-html-publish-to-html
+	 :section-numbers t
+	 :with-timestamps nil
+	 :time-stamp-file nil
+	 :html-use-infojs t
+	 :html-home/up-format ""
+	 :html-infojs-options (
+	 		      (path . "https://orgmode.org/org-info.js")
+	 		      (view . "info")
+	 		      (toc . :with-toc)
+	 		      (ftoc . "0")
+	 		      (tdepth . "max")
+	 		      (sdepth . "max")
+	 		      (mouse . "underline")
+	 		      (buttons . nil)
+	 		      (ltoc . "above")
+	 		      (up . :html-link-up)
+	 		      (home . :html-link-home))
+	 :html-allow-name-attribute-in-anchors t
+	 :html-prefer-user-labels t
+	 :base-directory "~/.emacs.d/"
+	 :publishing-directory "~/build/web/misc/emacs.d/"
+	 :recursive f
+	 )
 	)
       )
 (org-publish "notebook")
-(org-publish "dotfiles")
+(org-publish "dotfiles-hmtl")
+(org-publish "emacs.d-html")
