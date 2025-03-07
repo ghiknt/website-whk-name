403bb18b34092add200054a231ea92dce1b13ad4
https://whk.name/about/me/ (whk) <develop@whk.name>
2025-03-07 13:50:41 +0000

    Setup to use local copy of org-info-src.js instead of web copy of org-info.js

--- config/publish/index.el
+++ config/publish/index.el
@@ -1,27 +1,27 @@
 (setq org-publish-project-alist
       '(
 	("notebook-html"
-	 :base-extension "org"
-	 :publishing-function org-html-publish-to-html
-	 :section-numbers t
-	 :with-timestamps nil
-	 :time-stamp-file nil
-	 :html-use-infojs t
-	 :html-home/up-format ""
-	 :html-infojs-options (
-	 		      (path . "https://orgmode.org/org-info.js")
-	 		      (view . "info")
-	 		      (toc . :with-toc)
-	 		      (ftoc . "0")
-	 		      (tdepth . "max")
-	 		      (sdepth . "max")
-	 		      (mouse . "underline")
-	 		      (buttons . nil)
-	 		      (ltoc . "above")
-	 		      (up . :html-link-up)
-	 		      (home . :html-link-home))
-	 :html-allow-name-attribute-in-anchors t
-	 :html-prefer-user-labels t
+	   :base-extension "org"
+	   :publishing-function org-html-publish-to-html
+	   :section-numbers t
+	   :with-timestamps nil
+	   :time-stamp-file nil
+	   :html-use-infojs t
+	   :html-home/up-format ""
+	 ;;  :html-infojs-options (
+	 ;;			(path . "https://orgmode.org/org-info.js")
+	 ;;			(view . "info")
+	 ;;			(toc . :with-toc)
+	 ;;			(ftoc . "0")
+	 ;;			(tdepth . "max")
+	 ;;			(sdepth . "max")
+	 ;;			(mouse . "underline")
+	 ;;			(buttons . nil)
+	 ;;			(ltoc . "above")
+	 ;;			(up . :html-link-up)
+	 ;;			(home . :html-link-home))
+	   :html-allow-name-attribute-in-anchors t
+	   :html-prefer-user-labels t
 	 :base-directory "../.."
 	 :publishing-directory "~/build/web/notebook/"
 	 :recursive t
@@ -42,54 +42,54 @@
 				 "notebook-static"
 				 ))
 	("dotfiles-html"
-	 :base-extension "org"
-	 :publishing-function org-html-publish-to-html
-	 :section-numbers t
-	 :with-timestamps nil
-	 :time-stamp-file nil
-	 :html-use-infojs t
-	 :html-home/up-format ""
-	 :html-infojs-options (
-	 		      (path . "https://orgmode.org/org-info.js")
-	 		      (view . "info")
-	 		      (toc . :with-toc)
-	 		      (ftoc . "0")
-	 		      (tdepth . "max")
-	 		      (sdepth . "max")
-	 		      (mouse . "underline")
-	 		      (buttons . nil)
-	 		      (ltoc . "above")
-	 		      (up . :html-link-up)
-	 		      (home . :html-link-home))
-	 :html-allow-name-attribute-in-anchors t
-	 :html-prefer-user-labels t
+	   :base-extension "org"
+	   :publishing-function org-html-publish-to-html
+	   :section-numbers t
+	   :with-timestamps nil
+	   :time-stamp-file nil
+	   :html-use-infojs t
+	   :html-home/up-format ""
+	 ;;  :html-infojs-options (
+	 ;;			(path . "https://orgmode.org/org-info.js")
+	 ;;			(view . "info")
+	 ;;			(toc . :with-toc)
+	 ;;			(ftoc . "0")
+	 ;;			(tdepth . "max")
+	 ;;			(sdepth . "max")
+	 ;;			(mouse . "underline")
+	 ;;			(buttons . nil)
+	 ;;			(ltoc . "above")
+	 ;;			(up . :html-link-up)
+	 ;;			(home . :html-link-home))
+	   :html-allow-name-attribute-in-anchors t
+	   :html-prefer-user-labels t
 	 :base-directory "~/dotfiles/"
 	 :publishing-directory "~/build/web/misc/dotfiles/"
 	 :recursive f
 	 ;; 
 	 )
 	("emacs.d-html"
-	 :base-extension "org"
-	 :publishing-function org-html-publish-to-html
-	 :section-numbers t
-	 :with-timestamps nil
-	 :time-stamp-file nil
-	 :html-use-infojs t
-	 :html-home/up-format ""
-	 :html-infojs-options (
-	 		      (path . "https://orgmode.org/org-info.js")
-	 		      (view . "info")
-	 		      (toc . :with-toc)
-	 		      (ftoc . "0")
-	 		      (tdepth . "max")
-	 		      (sdepth . "max")
-	 		      (mouse . "underline")
-	 		      (buttons . nil)
-	 		      (ltoc . "above")
-	 		      (up . :html-link-up)
-	 		      (home . :html-link-home))
-	 :html-allow-name-attribute-in-anchors t
-	 :html-prefer-user-labels t
+	   :base-extension "org"
+	   :publishing-function org-html-publish-to-html
+	   :section-numbers t
+	   :with-timestamps nil
+	   :time-stamp-file nil
+	   :html-use-infojs t
+	   :html-home/up-format ""
+	 ;;  :html-infojs-options (
+	 ;;			(path . "https://orgmode.org/org-info.js")
+	 ;;			(view . "info")
+	 ;;			(toc . :with-toc)
+	 ;;			(ftoc . "0")
+	 ;;			(tdepth . "max")
+	 ;;			(sdepth . "max")
+	 ;;			(mouse . "underline")
+	 ;;			(buttons . nil)
+	 ;;			(ltoc . "above")
+	 ;;			(up . :html-link-up)
+	 ;;			(home . :html-link-home))
+	   :html-allow-name-attribute-in-anchors t
+	   :html-prefer-user-labels t
 	 :base-directory "~/.emacs.d/"
 	 :publishing-directory "~/build/web/misc/emacs.d/"
 	 :recursive f
