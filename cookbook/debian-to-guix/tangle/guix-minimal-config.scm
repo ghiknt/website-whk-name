152bb220e2f56058166f43bd8686416c369b9842
https://whk.name/about/me/ (whk) <develop@whk.name>
2025-12-29 21:35:51 +0000

    Update to use 1.5.0rc1

--- cookbook/debian-to-guix/tangle/guix-minimal-config.scm
+++ cookbook/debian-to-guix/tangle/guix-minimal-config.scm
@@ -6,12 +6,44 @@
 ;;   ssh enabled with password login disabled and root key set
 
 (use-modules (gnu))
-(use-service-modules networking)
+(use-service-modules networking ssh)
 
 (operating-system
- (host-name "uman001.erpetu.net")
- (timezone "UTC")
- (locale "en_US.utf8")
+  (locale "en_US.utf8")
+  (timezone "UTC")
+  (keyboard-layout (keyboard-layout "us"))
+  (host-name "uman001")
+
+  ;; The "root"
+  ;; account is implicit, and is initially created with the
+  ;; empty password.
+  ;; The list of user accounts ('root' is implicit).
+  (users (cons* %base-user-accounts))
+
+  ;; Packages installed system-wide.  Users can also install packages
+  ;; under their own account: use 'guix search KEYWORD' to search
+  ;; for packages and 'guix install PACKAGE' to install a package.
+  (packages (cons* %base-packages))
+
+  ;; Add services to the baseline: a DHCP client
+  (services (append (list
+		     ;; Use dhcpd for network information
+		     (service dhcpd-service-type)
+		     ;; Default firewall configuration.  Should
+		     ;; only allow port 22 (TODO: Make explicit)
+		     (service nftables-service-type)
+		     ;; Configure SSH
+		     (service openssh-service-type
+			      (openssh-configuration
+			       (permit-root-login 'prohibit-password)
+			       (password-authentication? #f)
+			       (public-key-authentication? #t)
+			       (x11-forwarding? #f)
+			       (authorized-keys
+			       `(("root",
+				  (plain-file "id-2025-whk-development-ed25519.pub"
+					      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIO8R7fnSOC91BZgrOPchusXT9faa4dsbKUmL/ZvTQp/F WHK (develop@whk.name) Development Key")))))))
+		    %base-services))
 
  ;; Boot in "legacy" BIOS mode, assuming /dev/sdX is the
  ;; target hard disk, and "my-root" is the label of the target
@@ -32,21 +64,8 @@
 		 (device (file-system-label "guixBoot"))
 		 (mount-point "/boot")
 		 (type "ext4"))
-		(file-system
-		 (device (file-system-label "guixVar"))
-		 (mount-point "/var")
-		 (type "ext4"))
 		%base-file-systems))
 
  (initrd-modules (cons "virtio_scsi"    ;needed to find the disk
-		       %base-initrd-modules))
-
- ;; The "root"
- ;; account is implicit, and is initially created with the
- ;; empty password.
-
- ;; Add services to the baseline: a DHCP client
- (services (append (list (service dhcp-client-service-type)
-			 (service nftables-service-type))
-		   %base-services)))
+		       %base-initrd-modules)))
 ;; guix-minimal-config.scm ends here
